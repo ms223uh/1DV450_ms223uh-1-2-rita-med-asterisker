087f1dd527f620541bf585eb2b2e31447b6d891a
Mathias Sundin <ms223uh@student.lnu.se>
2014-09-15 14:36:05 +0200

    Lab2 Done!

---
+++ lab 2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 25; i++)
+            {
+                switch (i % 3)
+                {  
+
+                    case 0:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        break;
+
+                    case 1:
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        break;
+
+                    case 2:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        break;
+                }
+
+                if (i % 2 == 1)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int a = 0; a < 39; a++)
+                {
+                    Console.Write("* ");
+                } Console.WriteLine();
+                
+            }
+            
+        }
+    }
+}
